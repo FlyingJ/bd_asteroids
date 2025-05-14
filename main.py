d425a78ec3af1fd2916fe3eaf81a7914d0451fe6
FlyingJ <jason.m.harrington@gmail.com>
2025-05-14 18:56:11 -0400

    Add first source files...constants and basic main

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+import constants as const
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {const.SCREEN_WIDTH}")
+    print(f"Screen height: {const.SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
