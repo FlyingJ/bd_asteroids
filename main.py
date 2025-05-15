9fc80ac82dffc8d59fd0da3817291286e44e195a
FlyingJ <jason.m.harrington@gmail.com>
2025-05-14 23:57:22 -0400

    Add basic pygame screen

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import pygame
 import constants as const
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {const.SCREEN_WIDTH}")
-    print(f"Screen height: {const.SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((const.SCREEN_WIDTH,const.SCREEN_HEIGHT))
+
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
+    # print("Starting Asteroids!")
+    # print(f"Screen width: {const.SCREEN_WIDTH}")
+    # print(f"Screen height: {const.SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
