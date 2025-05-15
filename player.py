2bb36025c1427723caa614bf5def7e5f12a2bedf
FlyingJ <jason.m.harrington@gmail.com>
2025-05-15 01:05:02 -0400

    Add rotate and update methods to Player

--- player.py
+++ player.py
@@ -1,8 +1,9 @@
-import circleshape
+import pygame
 
+import circleshape as cs
 import constants as const
 
-class Player(CircleShape):
+class Player(cs.CircleShape):
 
 	def __init__(self, x, y):
 		super().__init__(x, y, const.PLAYER_RADIUS)
@@ -18,9 +19,20 @@ class Player(CircleShape):
 	    c = self.position - forward * self.radius + right
 	    return [a, b, c]
 
-	def draw(screen):
+	def draw(self, screen):
 		pygame.draw.polygon(
 			screen,
 			"white",
 			self.triangle(),
 			2)
+
+	def rotate(self, dt):
+		self.rotation += const.PLAYER_TURN_SPEED * dt
+
+	def update(self, dt):
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_a]:
+			self.rotate(dt)
+		if keys[pygame.K_d]:
+			self.rotate(-dt)
