fe6f9a0de1c1d6377da15836c4127cf9cfef32ea
FlyingJ <jason.m.harrington@gmail.com>
2025-05-15 00:10:10 -0400

    Add QUIT event handler to exit game on window close

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     screen = pygame.display.set_mode((const.SCREEN_WIDTH,const.SCREEN_HEIGHT))
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill("black")
         pygame.display.flip()
     # print("Starting Asteroids!")
