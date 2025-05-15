7a4aa969cde2f5fffa8688a4630c910b3d76e08c
FlyingJ <jason.m.harrington@gmail.com>
2025-05-15 00:11:21 -0400

    Remove clutter comments

--- main.py
+++ main.py
@@ -12,9 +12,6 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-    # print("Starting Asteroids!")
-    # print(f"Screen width: {const.SCREEN_WIDTH}")
-    # print(f"Screen height: {const.SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
